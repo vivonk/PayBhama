395d369dd69d36ad324e7519ed1febc60ed2cd1a
M.L Bishnoi <30701209+iml-v@users.noreply.github.com>
2017-08-07 04:14:42 +0530

    SignUp changes

---
+++ app/src/main/java/inc/developer/vivonk/paybhama/Activities/SignUpActivity.java
@@ -0,0 +1,15 @@
+package inc.developer.vivonk.paybhama.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import inc.developer.vivonk.paybhama.R;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+    }
+}
