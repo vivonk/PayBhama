a19d876c52890a36380b321c631773d95eace0c4
Vivonk <nirmalsarswat400@gmail.com>
2017-08-06 17:35:35 +0530

    Created all fragments

--- app/src/main/java/inc/developer/vivonk/paybhama/fragments/PaymentFragment.java
+++ app/src/main/java/inc/developer/vivonk/paybhama/fragments/PaymentFragment.java
@@ -1,9 +1,29 @@
 package inc.developer.vivonk.paybhama.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.dlazaro66.qrcodereaderview.QRCodeReaderView;
+
 /**
  * Created by vivonk on 06-08-2017.
  */
 
 public class PaymentFragment extends Fragment{
-    
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
 }
