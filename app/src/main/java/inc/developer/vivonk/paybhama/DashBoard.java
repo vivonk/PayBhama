d5ba8794ae2b216350e4c95018c5a7b8d8e699a9
Vivonk <nirmalsarswat400@gmail.com>
2017-08-06 11:04:05 +0530

    Multi Language, Navigation complete

--- app/src/main/java/inc/developer/vivonk/paybhama/DashBoard.java
+++ app/src/main/java/inc/developer/vivonk/paybhama/DashBoard.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
 
@@ -20,14 +21,17 @@ public class DashBoard extends AppCompatActivity {
                 case R.id.navigation_home:
                     mTextMessage.setText(R.string.title_home);
                     return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                case R.id.navigation_history:
+                    mTextMessage.setText(R.string.history_hindi_english);
                     return true;
                 case R.id.navigation_notifications:
                     mTextMessage.setText(R.string.title_notifications);
                     return true;
                 case R.id.navigation_setting:
                     mTextMessage.setText(R.string.setting_hindi);
+                    return true;
+                case R.id.navigation_add_money:
+                    mTextMessage.setText(R.string.add_money_hindi);
                     return true;
             }
             return false;
@@ -45,4 +49,9 @@ public class DashBoard extends AppCompatActivity {
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 
+   /* @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        return super.onCreateOptionsMenu(menu);
+    }*/
 }
