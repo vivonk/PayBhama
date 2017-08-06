27572f0a9d53f72b2a724f66a0db09772e297208
Vivonk <nirmalsarswat400@gmail.com>
2017-08-06 13:23:58 +0530

    Start making fragments

--- app/src/main/java/inc/developer/vivonk/paybhama/dashboard/DashBoard.java
+++ app/src/main/java/inc/developer/vivonk/paybhama/dashboard/DashBoard.java
@@ -1,12 +1,15 @@
-package inc.developer.vivonk.paybhama;
+package inc.developer.vivonk.paybhama.dashboard;
 
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
+
+import inc.developer.vivonk.paybhama.R;
+import inc.developer.vivonk.paybhama.fragments.HomeFragment;
 
 public class DashBoard extends AppCompatActivity {
 
@@ -19,19 +22,24 @@ public class DashBoard extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+//                    mTextMessage.setText(R.string.title_home);
+                    getSupportActionBar().setTitle(R.string.app_name);
                     return true;
                 case R.id.navigation_history:
-                    mTextMessage.setText(R.string.history_hindi_english);
+//                    mTextMessage.setText(R.string.history_hindi_english);
+                    getSupportActionBar().setTitle(R.string.history_hindi_english);
                     return true;
                 case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                    getSupportActionBar().setTitle(R.string.title_notifications);
+//                    mTextMessage.setText(R.string.title_notifications);
                     return true;
                 case R.id.navigation_setting:
-                    mTextMessage.setText(R.string.setting_hindi);
+                    getSupportActionBar().setTitle(R.string.setting_hindi);
+//                    mTextMessage.setText(R.string.setting_hindi);
                     return true;
                 case R.id.navigation_add_money:
-                    mTextMessage.setText(R.string.add_money_hindi);
+                    getSupportActionBar().setTitle(R.string.add_money_hindi);
+//                    mTextMessage.setText(R.string.add_money_hindi);
                     return true;
             }
             return false;
@@ -44,9 +52,17 @@ public class DashBoard extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
+//        mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        if(findViewById(R.id.content)!=null){
+            if (savedInstanceState != null) {
+                return;
+            }
+            HomeFragment homeFragment=new HomeFragment();
+            FragmentManager fragmentManager= getSupportFragmentManager();
+            fragmentManager.beginTransaction().replace(R.id.content,homeFragment).commit();
+        }
     }
 
    /* @Override
---
+++ app/src/main/java/inc/developer/vivonk/paybhama/fragments/HomeFragment.java
@@ -0,0 +1,34 @@
+package inc.developer.vivonk.paybhama.fragments;
+
+import android.animation.Animator;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import inc.developer.vivonk.paybhama.R;
+
+/**
+ * Created by vivonk on 06-08-2017.
+ */
+
+public class HomeFragment extends android.support.v4.app.Fragment {
+    public HomeFragment() {
+        super();
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_home,container,false);
+    }
+}
