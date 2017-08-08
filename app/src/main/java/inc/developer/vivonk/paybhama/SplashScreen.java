01cad5a77098d8a0e0b9175d54fe3371390fb66a
M.L Bishnoi <30701209+iml-v@users.noreply.github.com>
2017-08-08 12:48:25 +0530

    SignUp changes

---
+++ app/src/main/java/inc/developer/vivonk/paybhama/SplashScreen.java
@@ -0,0 +1,13 @@
+package inc.developer.vivonk.paybhama;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+    }
+}
