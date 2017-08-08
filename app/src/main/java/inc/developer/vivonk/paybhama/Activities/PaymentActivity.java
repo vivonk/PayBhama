c515a285bd7b60e570c33e42544d28206635ad68
Vivonk <nirmalsarswat400@gmail.com>
2017-08-08 18:31:59 +0530

    UI complete

--- app/src/main/java/inc/developer/vivonk/paybhama/Activities/PaymentActivity.java
+++ app/src/main/java/inc/developer/vivonk/paybhama/Activities/PaymentActivity.java
@@ -1,14 +1,21 @@
 package inc.developer.vivonk.paybhama.Activities;
 
+import android.Manifest;
 import android.app.Activity;
+import android.content.pm.PackageManager;
 import android.graphics.PointF;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.util.Log;
 import android.widget.EditText;
 
 import com.google.zxing.Result;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import inc.developer.vivonk.paybhama.R;
 import me.dm7.barcodescanner.zxing.ZXingScannerView;
@@ -23,6 +30,7 @@ import static android.content.ContentValues.TAG;
 public class PaymentActivity extends Activity implements ZXingScannerView.ResultHandler {
     private ZXingScannerView barcodeScannerView;
     private EditText barCodeResult;
+    public static final int REQUEST_ID_MULTIPLE_PERMISSIONS = 1;
     /*@Override
     public void onQRCodeRead(String text, PointF[] points) {
         qrCodeText.setText(text);
@@ -33,6 +41,9 @@ public class PaymentActivity extends Activity implements ZXingScannerView.Result
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_payment);
         Log.e(TAG, "onCreate: Payment Actiity created" );
+        if(checkAndRequestPermissions()) {
+            // carry on the normal flow, as the case of  permissions  granted.
+        }
         barcodeScannerView=(ZXingScannerView) findViewById(R.id.barcode_reader);
         barCodeResult=(EditText)findViewById(R.id.barcode_result);
       //  barcodeScannerView.startCamera();
@@ -60,5 +71,22 @@ public class PaymentActivity extends Activity implements ZXingScannerView.Result
         barCodeResult.setText(result.getText());
         Log.e(TAG, "handleResult:   handle result is -------\n "+ result.getBarcodeFormat().toString());
         barcodeScannerView.resumeCameraPreview(this);
+    }
+    private  boolean checkAndRequestPermissions() {
+        int permissionInternet = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.INTERNET);
+        int permissionCamera = ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA);
+        List<String> listPermissionsNeeded = new ArrayList<>();
+        if (permissionInternet!= PackageManager.PERMISSION_GRANTED) {
+            listPermissionsNeeded.add(Manifest.permission.INTERNET);
+        }
+        if (permissionCamera != PackageManager.PERMISSION_GRANTED) {
+            listPermissionsNeeded.add(Manifest.permission.CAMERA);
+        }
+        if (!listPermissionsNeeded.isEmpty()) {
+            ActivityCompat.requestPermissions(this, listPermissionsNeeded.toArray(new String[listPermissionsNeeded.size()]),REQUEST_ID_MULTIPLE_PERMISSIONS);
+            return false;
+        }
+        return true;
     }
 }
--- app/src/main/java/inc/developer/vivonk/paybhama/SplashScreen.java
+++ app/src/main/java/inc/developer/vivonk/paybhama/SplashScreen.java
@@ -1,12 +1,18 @@
 package inc.developer.vivonk.paybhama;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
@@ -15,6 +21,7 @@ import inc.developer.vivonk.paybhama.Activities.loginOrSignUpActivity;
 
 public class SplashScreen extends AppCompatActivity {
     private static final String TAG ="TAG" ;
+    public static final int REQUEST_ID_MULTIPLE_PERMISSIONS = 1;
     Button mProceedToLogin,mProceedForWebView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,5 +51,18 @@ public class SplashScreen extends AppCompatActivity {
                 startActivity(new Intent(getApplicationContext(),loginOrSignUpActivity.class));
             }
         });
+        int permissionInternet = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.INTERNET);
+        int permissionCamera = ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA);
+        List<String> listPermissionsNeeded = new ArrayList<>();
+        if (permissionInternet!= PackageManager.PERMISSION_GRANTED) {
+            listPermissionsNeeded.add(Manifest.permission.INTERNET);
+        }
+        if (permissionCamera != PackageManager.PERMISSION_GRANTED) {
+            listPermissionsNeeded.add(Manifest.permission.CAMERA);
+        }
+        if (!listPermissionsNeeded.isEmpty()) {
+            ActivityCompat.requestPermissions(this, listPermissionsNeeded.toArray(new String[listPermissionsNeeded.size()]),REQUEST_ID_MULTIPLE_PERMISSIONS);
+        }
     }
 }
